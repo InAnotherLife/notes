fbe76d8996f389fdd9a5edd8459debb0803c8b01
Faye Billye <fbillye@student.21-school.ru>
2023-10-08 17:46:08 +0700

    Update menu, update README.md

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
 #include "notes.h"
 
 int main() {
-  my::Notes notes;
-  int menu;
   std::cout << "Программа \"Заметки\"" << std::endl;
   std::cout << std::endl;
+  my::Notes notes;
+  notes.LoadNotes();
+  size_t menu;
   while (true) {
     notes.ShowMenu();
     std::cin >> menu;
     std::cout << "Выбран пункт меню: " << menu << std::endl;
     if (menu == 1) {
-      notes.LoadNotes();
-    } else if (menu == 2) {
       notes.ShowNotes();
-    } else if (menu == 3) {
+    } else if (menu == 2) {
       notes.CreateNote();
-    } else if (menu == 4) {
+    } else if (menu == 3) {
       notes.EditNote();
-    } else if (menu == 5) {
+    } else if (menu == 4) {
       notes.DelNote();
-    } else if (menu == 6) {
+    } else if (menu == 5) {
       notes.SaveNotes();
     } else if (menu == 0) {
       std::cout << "Выход из программы." << std::endl;
--- src/notes.cpp
+++ src/notes.cpp
@@ -4,12 +4,11 @@ namespace my {
 
 void Notes::ShowMenu() {
   std::cout << "Выберите пункт меню: " << std::endl;
-  std::cout << "1. Загрузить заметки из файла" << std::endl;
-  std::cout << "2. Просмотреть заметки" << std::endl;
-  std::cout << "3. Создать заметку" << std::endl;
-  std::cout << "4. Редактировать заметку" << std::endl;
-  std::cout << "5. Удалить заметку" << std::endl;
-  std::cout << "6. Сохранить заметки в файл" << std::endl;
+  std::cout << "1. Просмотреть заметки" << std::endl;
+  std::cout << "2. Создать заметку" << std::endl;
+  std::cout << "3. Редактировать заметку" << std::endl;
+  std::cout << "4. Удалить заметку" << std::endl;
+  std::cout << "5. Сохранить заметки в файл" << std::endl;
   std::cout << "0. Выйти из программы" << std::endl;
   std::cout << std::endl;
 }
